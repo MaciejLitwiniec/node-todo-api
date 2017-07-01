17e86e2b524e51f107a362e59b80c63e237ce922
MaciejLitwiniec <litwiniec.maciej@gmail.com>
2017-07-01 22:09:53 +0200

    added delete methods

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -23,6 +23,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log(result);
   // });
 
+  // //look for any duplicates => name, use deleteMany
+  // db.collection('Users').deleteMany({name: 'Maciej'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  //find one and delete, doesnt matter which one, but should be deleted by ID
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID('5957d91db1e628203021d9a4'
+  )}).then((result) => {
+    console.log(JSON.stringify(result, undefined, 2));
+  });
 
   // db.close();
 });
