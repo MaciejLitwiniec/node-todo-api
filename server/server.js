2499449879a134e5b92d2c154994423d81f0039b
MaciejLitwiniec <litwiniec.maciej@gmail.com>
2017-07-16 19:07:35 +0200

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,28 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+//GET /todos/1234567
+//:id is url pattern for dynamic resource queries
+//id will be a variable created on request object
+//later we wil be able to access that variable
+app.get('/todos/:id', (req, res) => {
+  //req.params provides an object with key value pairs
+  //where id is a key and what was delivered in URL will be a value
+  //res.send(req.params);
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => res.status(404).send());
 });
 
 app.listen(3000, () => {
