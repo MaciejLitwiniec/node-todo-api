c37e2641cee705fe1e67c98addf9c165615d3f0f
MaciejLitwiniec <litwiniec.maciej@gmail.com>
2017-07-16 10:47:05 +0200

    new queries in playground folder

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,18 +1,47 @@
+//we will use it to validate object id
+const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
 
-var id = '595d32e3953caf2d9c983afe';
+//var uid = '5958e2d2d3e9412d8020a088';
 
-//returns array
-Todo.find({
-  _id: id
-}).then((todos) => {
-  console.log('Todos', todos);
-});
+// var id = '595d32e3953caf2d9c983afe11';
+//
+// //by flipping ObjectID obtain 'when ObjectID is invalid'
+// if (!ObjectID.isValid(id)) {
+//   console.log('id not valid');
+// }
 
-//returns document
-Todo.findOne({
-  _id: id
-}).then((todo) => {
-  console.log('Todo', todo);
-});
+// //returns array, or empty array
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// //returns document, or null
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// //returns document, or null
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('ID not found');
+//   }
+//   console.log('Todo by id', todo);
+// }).catch((e) => console.log(e));
+
+//challenge
+//User.findById
+//case: where the query works, but there is no user (like above), "user not found"
+User.findById('5958e2d2d3e9412d8020a088').then((user) => {
+  if (!user) {
+    return console.log('user not found');
+  }
+  console.log(JSON.stringify(user, undefined, 2));
+}).catch((e) => console.log(e));
